7943334f04d641d917aea809803f5832bba1f4ee
Chuvikovsky <chuvikovsky@gmail.com>
2025-04-16 16:36:20 +0300

    add rollbar into project

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -5,6 +5,7 @@ import App from "./components/App";
 import resources from "./locales/index.js";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -14,13 +15,23 @@ const init = async () => {
     fallbackLng: "ru",
   });
 
+  const rollbarConfig = {
+    accessToken:
+      "2f2704ebf6cb4eeeb7bbe8cbfbbf6b818c233c36657f734c6306f55c4cc4b315ca87f65cf3d53dff7ce07d9f4b61974c",
+    environment: "testenv",
+  };
+
   return (
     <StrictMode>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </StrictMode>
   );
 };
--- frontend/src/rollbar_index.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./rollbar";
-
-const app = () => {
-  createRoot(document.getElementById("chat")).render(<App />);
-};
-
-app();
